ab3ae5398d2213839494fb3d696a0867f1c6c2a7
unknown <kaba100@yandex.ru>
2021-03-15 09:37:49 +0700

    try one

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const body = document.body;
 const cardArea = document.createElement('div');
-const home = document.getElementById('home');
 const cards = document.querySelectorAll('.card-area');
 
 let level = 3;
 document.querySelectorAll('.level__choice-item').forEach(item => item.addEventListener('click', levelChoice));
 
-function backHome() {
-  body.appendChild(home);
-  cardArea.innerHTML = '';
-  cardArea.remove();
-}
 
 function levelChoice (event) {
   level = +event.target.getAttribute('data-level');
@@ -27,6 +21,13 @@ function start() {
   document.body.innerHTML = '';
   document.body.append(cardArea);
   cardArea.classList.add('card-area');
+  if (level === 3) {
+    cardArea.classList.add('card-area-three');
+  } else if (level === 6) {
+    cardArea.classList.add('card-area-six');
+  } else if (level === 9) {
+    cardArea.classList.add('card-area-nine');
+  }
   for (let i = 0; i < level; i++) {
     createCard();
   }
@@ -37,7 +38,7 @@ function createCard() {
   card.innerHTML = `<div class='flip-card'>
   <div class='flip-card-inner'>
     <div class='flip-card-front'>
-      <img src='images/t-shirt.png' alt='card' class = 'image'> 
+      <img src='images/t-shirt.png' alt='card'> 
     </div>
     <div class='flip-card-back'>
       <img src='images/game-over.png' alt='game-over' class = 'image'>
@@ -48,17 +49,16 @@ function createCard() {
   card.addEventListener('click', rotateCard);
 }
 
+let gameOver = false;
 
 function rotateCard(event) {
-  const random = Math.floor(Math.random() * level);
-  console.log(random);
-  event.target.parentElement.parentElement.classList.add('rotate');
+  if (!gameOver) {
+    const random = Math.floor(Math.random() * level);
+    document.querySelectorAll('.image')[random].src = 'images/bug.png';
+    event.target.parentElement.parentElement.classList.add('rotate');
+    gameOver = true;
+  } else {
+    setTimeout(() => window.location.reload(), 1000);
+  }
 }
 
-function turnedOverCard() {
-  this.classList.toggle('rotate');
-  let allCards = document.querySelectorAll('.card-area');
-  allCards.forEach((item) => item.addEventListener('click', backHome));
-}
-
-// div.flip-card-inner
